82ae28c6fb185ad1caaeec83c9c60e50d7c4643b
DanielPolus <daniel.poliuskevych@gmail.com>
2023-04-11 23:57:26 +0300

    test brunch commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,24 +8,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Set<String> gods = new HashSet<String>();
-        Set<String> planets = new HashSet<String>();
-        Set<String> sweets = new HashSet<String>();
-        Set<String> result = new HashSet<String>();
-
-        gods.add("Mercury");
-        gods.add("Mars");
-
-        planets.add("Mercury");
-        planets.add("Mars");
-
-        sweets.add("Mars");
-
-        gods.retainAll(sweets);
-        gods.retainAll(planets);
-
-        result = gods;
-        System.out.println("!!!!" + result.toString() + "!!!!");
+        System.out.println("hhehehehehe i deleted ur code :3");
     }
 
 }
