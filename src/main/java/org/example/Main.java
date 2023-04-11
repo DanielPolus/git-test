5cb96fd7fbe250b416ac3e8fa975db86a4f66764
Даня <during2105@gmail.com>
2023-04-11 17:30:35 +0300

    Init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.HashSet;
+import java.util.Set;
+public class Main {
+
+
+    public static void main(String[] args) {
+        Set<String> gods = new HashSet<String>();
+        Set<String> planets = new HashSet<String>();
+        Set<String> sweets = new HashSet<String>();
+        Set<String> result = new HashSet<String>();
+
+        gods.add("Mercury");
+        gods.add("Mars");
+
+        planets.add("Mercury");
+        planets.add("Mars");
+
+        sweets.add("Mars");
+
+        gods.retainAll(sweets);
+        gods.retainAll(planets);
+
+        result = gods;
+        System.out.println("!!!\n" + result.toString() + "\n!!!");
+
+    }
+
+}
